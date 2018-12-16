b6e1478134225400d46bddcf3de974b385745cc1
Lakty <la.v@i.ua>
2018-12-16 20:07:50 +0200

    Добавил первый хендлер

--- bot.py
+++ bot.py
@@ -1,17 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import os
-import telegram
-from telegram.ext import Updater
-from telegram.ext import MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
 TOKEN = "560289646:AAFzO2loYla3rOfMPBT_1S9YihU1JYN6dBU"
 PORT = int(os.environ.get('PORT', '8443'))
-updater = Updater(TOKEN)
-dispatcher = updater.dispatcher
+
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+
+def start(bot, update):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi!')
+
+
+def help(bot, update):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+
 def echo(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
-updater.start_webhook(listen="0.0.0.0",
-                      port=PORT,
-                      url_path=TOKEN)
-updater.bot.set_webhook("https://hnuvs.herokuapp.com/" + TOKEN)
-echo_handler = MessageHandler(Filters.text, echo)
-dispatcher.add_handler(echo_handler)
-updater.idle()
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+
+def error(bot, update, error):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, error)
+
+
+def main():
+
+    updater = Updater(TOKEN)
+
+
+    dispatcher = updater.dispatcher
+
+    updater.start_webhook(listen="0.0.0.0",
+                          port=PORT,
+                          url_path=TOKEN)
+    updater.bot.set_webhook("https://hnuvs.herokuapp.com/" + TOKEN)
+
+    # on different commands - answer in Telegram
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", help))
+
+
+    # log all errors
+    dispatcher.add_error_handler(error)
+
+    # Start the Bot
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
