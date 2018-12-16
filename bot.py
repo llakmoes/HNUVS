ea94f8fc5e22e25ad01526beba20b5bab5b30468
Lakty <la.v@i.ua>
2018-12-16 20:27:34 +0200

    Ну заработай, пожалуйста

--- bot.py
+++ bot.py
@@ -1,57 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
-from telegram.ext import Updater, CommandHandler
-import logging
+from telegram.ext import Updater, MessageHandler, Filters
+
 TOKEN = '560289646:AAFzO2loYla3rOfMPBT_1S9YihU1JYN6dBU'
 PORT = int(os.environ.get('PORT', '5000'))
 
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
-
-logger = logging.getLogger(__name__)
-
-
-
-def start(bot, update):
-    """Send a message when the command /start is issued."""
-    update.message.reply_text('Hi!')
-
-
-def help(bot, update):
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
-
-
 def echo(bot, update):
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
+    update.message.reply_text('Bot answer: ' + update.message.text)
 
 
-def error(bot, update, error):
-    """Log Errors caused by Updates."""
-    logger.warning('Update "%s" caused error "%s"', update, error)
+updater = Updater(TOKEN)
 
+# add handlers
+updater.dispatcher.add_handler(MessageHandler(Filters.text, echo))
 
-def main():
-
-    updater = Updater(TOKEN)
-
-    dispatcher = updater.dispatcher
-
-    updater.start_webhook(listen="0.0.0.0",
-                          port=PORT,
-                          url_path=TOKEN)
-    updater.bot.set_webhook("https://hnuvs.herokuapp.com/" + TOKEN)
-
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("help", help))
-
-    dispatcher.add_error_handler(error)
-
-    updater.idle()
-
-
-if __name__ == '__main__':
-    main()
+updater.start_webhook(listen="0.0.0.0",
+                      port=PORT,
+                      url_path=TOKEN)
+updater.bot.setWebhook("https://hnuvs.herokuapp.com/" + TOKEN)
+updater.idle()
